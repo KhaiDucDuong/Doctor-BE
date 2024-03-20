f57e8c4eb386d4b4f405c17c41f2d043cfbc7953
hoangtran0935 <21110031@student.hcmute.edu.vn>
2024-03-21 00:48:10 +0700

    Update Appointment
    Update controller

--- src/main/java/com/example/doctor/appointment/appointmentController/AppointmentController.java
+++ src/main/java/com/example/doctor/appointment/appointmentController/AppointmentController.java
@@ -1,10 +1,38 @@
 package com.example.doctor.appointment.appointmentController;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.doctor.appointment.Appointment;
+import com.example.doctor.appointment.appointmentApplication.AppointmentApplication;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.SneakyThrows;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/appointment")
 public class AppointmentController {
-
+    @Autowired
+    private AppointmentApplication appointmentApplication;
+    @Autowired
+    private HttpServletRequest request;
+    @SneakyThrows
+    @GetMapping("/booking/{data}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Appointment CreateAppointment(@PathVariable("data") ObjectId data, Date date, @RequestBody Appointment appointment) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            ObjectId userId = (ObjectId) session.getAttribute("userId");
+            if (userId != null) {
+                appointment.setPatientId(userId);
+                appointment.setScheduleId(data);
+                appointment.setDate(date);
+                appointmentApplication.createAppointmentByBooking(appointment);
+            }
+        }
+        return appointment;
+    }
 }
